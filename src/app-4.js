bf16efa7ab4a4a3e7f1ada812ef3e97a91a25a89
Todd Morningstar <todd@thoughtbot.com>
2018-06-30 12:06:21 -0700

    Implement up to step 5 in Vue.js Demo
    * From step 5, [handling user
    input](https://vuejs.org/v2/guide/#Handling-User-Input)

---
+++ src/app-4.js
@@ -0,0 +1,13 @@
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+});
+
+app4.todos.push({ text: 'New item - added to array of TODOs' });
+
